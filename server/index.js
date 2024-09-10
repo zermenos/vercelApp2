73a2684a41373c8369dc7dd21b2e5b6d7ea9801a
Sebastian <sebastian.21@outlook.com>
2024-09-09 22:52:02 -0600

    new

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import getRawBody from "raw-body";
 import path from "path";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
