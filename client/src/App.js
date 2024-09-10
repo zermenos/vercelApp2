117044ab2f2bba616fb0e529a1a722a1f2bc5236
Sebastian <sebastian.21@outlook.com>
2024-09-10 16:12:36 -0600

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import openURL from "@stdlib/utils-open-url";
 
 //const base_url = window.location.origin + window.location.pathname;
 
@@ -28,17 +29,15 @@ function Button() {
         <button
           className="btn-qr"
           onClick={() => {
-            fetch(
-              /*base_url + */ "https://vercelapp2-backend.onrender.com/api/sign-in",
-              {
-                mode: "cors",
-              }
-            )
+            fetch("https://vercelapp2-backend.onrender.com/api/sign-in", {
+              mode: "cors",
+            })
               .then((r) =>
                 Promise.all([Promise.resolve(r.headers.get("x-id")), r.json()])
               )
               .then(([id, data]) => {
                 console.log(data);
+                Verify();
                 return id;
               })
 
@@ -49,6 +48,51 @@ function Button() {
         </button>
       </div>
     </>
+  );
+}
+
+function Verify() {
+  const hostUrl = "https://vercelapp2-frontend.onrender.com";
+  const callbackURL = "/api/callback";
+  const sessionId = 1;
+  const verificationRequest = {
+    backUrl: hostUrl,
+    finishUrl: hostUrl,
+    logoUrl: hostUrl,
+    name: "Everi",
+    zkQueries: [
+      {
+        circuitId: "credentialAtomicQuerySigV2",
+        id: 1724187394,
+        query: {
+          allowedIssuers: [
+            "*",
+            //"did:iden3:privado:main:2ScrbEuw9jLXMapW3DELXBbDco5EURzJZRN1tYj7L7",
+          ],
+          context:
+            "https://raw.githubusercontent.com/anima-protocol/claims-polygonid/main/schemas/json-ld/pol-v1.json-ld",
+          type: "AnimaProofOfLife",
+          credentialSubject: {
+            human: {
+              $eq: true,
+            },
+          },
+        },
+      },
+    ],
+
+    callbackUrl: `${hostUrl}${callbackURL}?sessionId=${sessionId}`,
+    //audience,
+    verifierDid:
+      "did:polygonid:polygon:amoy:2qQ68JkRcf3xrHPQPWZei3YeVzHPP58wYNxx2mEouR",
+  };
+  // Encode the verification request to base64
+  const base64EncodedVerificationRequest = btoa(
+    JSON.stringify(verificationRequest)
+  );
+  return (
+    // Open the Polygon ID Verification Web Wallet with the encoded verification request
+    openURL(`https://wallet.privado.id/#${base64EncodedVerificationRequest}`)
   );
 }
 
